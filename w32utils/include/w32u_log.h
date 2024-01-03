82ef30f11dca8c15a11e3c199aae31f139c8fe97
emanuele <me.emanuele.xyz@gmail.com>
2024-01-03 13:27:46 +0100

    add log documentation

--- w32utils/include/w32u_log.h
+++ w32utils/include/w32u_log.h
@@ -12,7 +12,15 @@ typedef enum w32u_log_lvl
 	W32U_LOG_LVL_TRACE,
 } w32u_log_lvl;
 
-// TODO: Add documentation
+/*
+	w32u_logger is used for configuring logging.
+	If logger.file is a valid handle to a file, we log to the file.
+	If logger.console_out and logger.console_err are both valid console handles, we log to the console.
+	The former is used for standard output and the latter is used for standard error.
+	If logger.debug is not zero, we log using OutputDebugString.
+	We only log messages whose level is up to logger.level, included.
+	Set logger.level to -1 to disable logging entirely.
+*/
 typedef struct w32u_logger
 {
 	HANDLE file;
@@ -22,7 +30,12 @@ typedef struct w32u_logger
 	int level;
 } w32u_logger;
 
-// TODO: Add documentation
+/*
+	Log a message.
+	logger: struct used to configure logging.
+	level: log level for the message. Clamped between the min and max valid log levels.
+	msg: log message. If it's not valid, we log "".
+*/
 void w32u_log(w32u_logger logger, w32u_log_lvl level, const char* msg);
 
 #define w32u_crash(logger, msg) w32u_log(logger, W32U_LOG_LVL_CRASH, msg)
