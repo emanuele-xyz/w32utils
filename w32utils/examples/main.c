c159cccce3b36ec691708884b467915509ce1bee
emanuele <me.emanuele.xyz@gmail.com>
2024-01-10 22:23:45 +0100

    setup separate project for each example

--- w32utils/examples/main.c
+++ w32utils/examples/main.c
@@ -1,24 +1,6 @@
 #include "w32u.h"
 #include <stdlib.h>
 
-void w32u_log_example(void)
-{
-	// NOTE: when opening a file or getting std handles, you should check for errors
-	w32u_logger logger = { 0 };
-	logger.file = CreateFileA("log.txt", GENERIC_WRITE, 0, 0, CREATE_ALWAYS, FILE_ATTRIBUTE_NORMAL, 0);;
-	logger.console_out = GetStdHandle(STD_OUTPUT_HANDLE);
-	logger.console_err = GetStdHandle(STD_ERROR_HANDLE);
-	logger.debug = 1;
-	logger.level = W32U_LOG_LVL_TRACE;
-
-	w32u_crash(logger, "Hello!");
-	w32u_error(logger, "Hello!");
-	w32u_warn(logger, "Hello!");
-	w32u_info(logger, "Hello!");
-	w32u_debug(logger, "Hello!");
-	w32u_trace(logger, "Hello!");
-}
-
 LRESULT window_proc(HWND hwnd, UINT msg, WPARAM wparam, LPARAM lparam)
 {
 	w32u_msg_buf* msg_buf = (w32u_msg_buf*)GetWindowLongPtrA(hwnd, GWLP_USERDATA);
@@ -88,12 +70,67 @@ void w32u_window_example(void)
 			}
 		}
 	}
+
+	free(window_msg_buf.buf);
+	CloseWindow(window);
+	UnregisterClassA(class_name, 0);
+}
+
+void w32u_input_state_example(void)
+{
+	w32u_logger logger = { 0 };
+	logger.file = INVALID_HANDLE_VALUE;
+	logger.console_out = INVALID_HANDLE_VALUE;
+	logger.console_err = INVALID_HANDLE_VALUE;
+	logger.debug = 1;
+	logger.level = W32U_LOG_LVL_TRACE;
+
+	BOOL is_dpi_aware = w32u_make_dpi_aware();
+	if (!is_dpi_aware) w32u_show_error_popup("Failed to set DPI awareness");
+
+	const char* class_name = "my_window_class_name";
+	ATOM class_registered = w32u_register_window_class(class_name);
+	if (!class_registered) w32u_show_error_popup("Failed to register window class");
+
+	w32u_msg_buf window_msg_buf = { 0 };
+	window_msg_buf.capacity = 256;
+	window_msg_buf.buf = calloc(window_msg_buf.capacity, sizeof(w32u_msg));
+	HWND window = w32u_create_window(class_name, "Window", 1280, 720, WS_OVERLAPPEDWINDOW, &window_msg_buf, window_proc);
+	if (!window) w32u_show_error_popup("Failed create window");
+
+	int is_running = 1;
+	while (is_running)
+	{
+		w32u_clear_msg_buf(&window_msg_buf);
+
+		{
+			MSG msg = { 0 };
+			while (PeekMessageA(&msg, 0, 0, 0, PM_REMOVE))
+			{
+				TranslateMessage(&msg);
+				DispatchMessageA(&msg);
+			}
+		}
+
+		for (int i = 0; i < window_msg_buf.size; i++)
+		{
+			w32u_msg msg = window_msg_buf.buf[i];
+			if (msg.msg == WM_CLOSE)
+			{
+				is_running = 0;
+			}
+		}
+	}
+
+	free(window_msg_buf.buf);
+	CloseWindow(window);
+	UnregisterClassA(class_name, 0);
 }
 
 int main(void)
 {
-	w32u_log_example();
 	w32u_window_example();
+	w32u_input_state_example();
 
 	return 0;
 }
